57f209604c543fa76b80c8d949cf819ccdc88a6e
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-10-11 16:24:30 +0700

    error: bugs jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,43 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpForce;
+    private float horizontal;
+    private bool isFacingRight;
+
+    private float y;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     private Rigidbody2D rb;
     private Animator anim;
-
-    private float horizontal;
-    private float vertical;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        y = transform.position.y;
     }
 
     private void Update()
     {
-        Movement();
-        Attack();  
+        PlayerMove();
+        Jump();
+        Flip();
     }
 
-    private void Movement()
+    private void PlayerMove()
     {
         horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
-        anim.SetBool("walk_front", vertical == -1);
-        anim.SetBool("walk_back", vertical == 1);
+        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+
+        anim.SetBool("walk", horizontal != 0);
+
     }
 
-    public void Attack()
+    private void Flip()
     {
-        if (Input.GetMouseButton(0))
+        if (isFacingRight && horizontal > 0 || !isFacingRight && horizontal < 0)
         {
-            anim.SetTrigger("attack_front");
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.SetBool("jump", transform.position.y > y);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
     }
 }
