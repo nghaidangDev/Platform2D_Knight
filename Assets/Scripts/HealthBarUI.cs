7bd1ed3208249ad4bbf31109cbe02248fb1b4f4e
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-10-13 15:10:19 +0700

    feat: Map Level_01 and UI HealthBar

---
+++ Assets/Scripts/HealthBarUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public static HealthBarUI instance;
+    [SerializeField] private Image currentHealthImg;
+    private float currentHealth = 100;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        UpdateCurrentHealth();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        UpdateCurrentHealth();
+    }
+
+    private void UpdateCurrentHealth()
+    {
+        currentHealthImg.fillAmount = currentHealth / 100;
+    }
+}
