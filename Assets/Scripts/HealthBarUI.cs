7584c1c1f65f8b3d8ceee267020650c9f88908c0
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-10-23 20:10:59 +0700

    feat: deleted PlayerMovement change = PlayerController and attack base for Player

--- Assets/Scripts/HealthBarUI.cs
+++ Assets/Scripts/HealthBarUI.cs
@@ -5,35 +5,5 @@ using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
-    public static HealthBarUI instance;
-    [SerializeField] private Image currentHealthImg;
-    public float currentHealth = 100;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void Update()
-    {
-        UpdateCurrentHealth();
-    }
-
-    public void TakeDamage(float damage)
-    {
-        currentHealth -= damage;
-        UpdateCurrentHealth();
-    }
-
-    private void UpdateCurrentHealth()
-    {
-        currentHealthImg.fillAmount = currentHealth / 100;
-    }
+    
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,5 +5,120 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private float horizontal;
+    private float coolDownTimer = 0f;
+
+
+    [SerializeField] private float speed;
+    [SerializeField] private float forceJump;
+    [SerializeField] private float attackTimer;
+
+    [SerializeField] private float checkRadiousGrounded;
+    [SerializeField] private float checkRadiousAttacked;
+
+    private bool isGrounded;
+    private bool isFacingRight;
+    private bool canJumpDouble;
+
+    [SerializeField] private Transform checkGround;
+    [SerializeField] private Transform checkAttack;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask enemyLayer;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        CanMovement();
+        CanJump();
+        CanAttack();
+        UpdateAnimations();
+        CheckAnyThing();
+        Flip();
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("move", horizontal != 0);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void CanMovement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void CanJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, forceJump);
+        }
+    }
+
+    private void CanAttack()
+    {
+        if (coolDownTimer <= 0)
+        {
+            if (Input.GetMouseButton(0) && isGrounded)
+            {
+                Collider2D[] enemyInRange = Physics2D.OverlapCircleAll(checkAttack.position, checkRadiousAttacked, enemyLayer);
+
+                foreach (Collider2D collider in enemyInRange)
+                {
+                    //Take damage
+                }
+                Debug.Log("Attack");
+                coolDownTimer = attackTimer;
+                anim.SetTrigger("attack");
+            }
+        }
+        else
+        {
+            coolDownTimer -= Time.deltaTime;
+
+            if (coolDownTimer <= 0)
+            {
+                coolDownTimer = 0;
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal > 0 || !isFacingRight && horizontal < 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void CheckJumpDouble()
+    {
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+            canJumpDouble = true;
+    }
+
+    private void CheckAnyThing()
+    {
+        isGrounded = Physics2D.OverlapCircle(checkGround.position, checkRadiousGrounded, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(checkGround.position, checkRadiousGrounded);
+
+        Gizmos.DrawSphere(checkAttack.position, checkRadiousAttacked);
+    }
 }
